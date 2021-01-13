c766d3dbab923d8b78debc9f08dff4cce183a837
taka-0 <taka.08070309@gmail.com>
2021-01-13 15:50:50 +0900

    一部修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
+  "env": {
+    "browser": true,
+    "es6": true
   },
-  extends: ["eslint:recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    Vue: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    indent: ["error", 2],
-  },
+  "extends": ["eslint:recommended"],
+  "plugins": ["eslint-plugin-html"],
+  "parserOptions": {
+    "ecmaVersion": 2018
+  }
 }
