aab82b852dd45b0af7b7b6501846dec359022f45
taka-0 <taka.08070309@gmail.com>
2021-01-16 12:21:57 +0900

    □と〇

---
+++ 1_workspace/1/□と○/main.js
@@ -0,0 +1,20 @@
+// const figure = document.getElementById("figure")
+// const squareButton = document.getElementById("square-button")
+// const circleButton = document.getElementById("circle-button")
+
+
+// circleButton.onclick = function (){
+//   // figure に rounded クラスを追加する
+//   figure.classList.add('rounded')
+// }
+
+// squareButton.onclick = function (){
+//   // figure から rounded クラスを削除する
+//   figure.classList.remove('rounded')
+// }
+
+const figure = document.getElementById("figure")
+
+figure.onclick = function (){
+    figure.classList.toggle('rounded')
+}
