d85ea2982679887508238805d7e2c5a735380603
taka-0 <taka.08070309@gmail.com>
2021-01-16 17:10:03 +0900

    3の倍数

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -15,16 +15,44 @@
 
 // 答え
 
-const genkiFunction = function(number){
-  for (let n=1; n<=number; n++) {
-    if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
-    } else {
-      console.log(n)
-    }
-  }
-}
-genkiFunction(24)
+// const genkiFunction = function(number){
+//   for (let n=1; n<=number; n++) {
+//     if (n % 3 === 0) {
+//       console.log(`${n}!!!!!!!`)
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+// genkiFunction(24)
 
 // 関数を指定した方が、応用が利く（一般化）
 // genkiFunction(100)
+
+
+const FizzBuzz =function(number){
+  for(let n=1; n<number; n++){
+    if (n%3===0 && n%5===0){
+      console.log("FizzBuzz");
+    }
+
+    else if(n%3===0){
+      console.log("Fizz");
+    }
+    else if(n%5===0){
+      console.log("Buzz");
+      
+    }
+    else {
+      console.log(n);
+    }
+   }
+}
+
+FizzBuzz(100)
+
+
+const genkiFunction3 =function(number){
+
+
+}
